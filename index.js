ff2d7a3451dee2ad3d73dc26afe322baa80c8b1d
glorio <69961033+gloriohenry@users.noreply.github.com>
2020-12-02 11:34:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let title = document.getElementsByTagName('h1').innerText;
+title = document.getElementById('header 2').innerText = "Student";
+console.log(title);
+
+
+
+
+function Website(){
+
+    document.getElementsByTagName("h1").innerText = value;
+    document.querySelector(".td").style.fontFamily = "Times New Roman";
+    document.querySelector(".td").style.color = ".greenlime";
+    document.querySelector(".td").style.fontSize = "3em";
+    document.querySelector(".td").style.backgroundColor ="black";
+
+    document.querySelector(".h1").style.backgroundColor = "green";
+    document.querySelector(".body").style.position = "absolute";
+    document.querySelector(".body").style.fontSize = "5em";
+
+    document.querySelector(".a").style.Color = "darkblue";
+    document.querySelector(".a").style.fontSize = "40px";
+
+    let span = document.getElementsByTagName("span") [0];
+    span.style.color="black";
+}
